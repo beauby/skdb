9d0c69e8fa3e912f43571f082ec9abb4c63359c8
Lucas Hosseini <lucas@skiplabs.io>
2024-03-15 11:18:12 +0100

    [runtime] Fix inconsistencies in preamble declarations.

--- compiler/runtime/palloc.c
+++ compiler/runtime/palloc.c
@@ -199,7 +199,7 @@ void SKIP_cond_wait(pthread_cond_t* x, pthread_mutex_t* y) {
   pthread_cond_wait(x, y);
 }
 
-int SKIP_cond_timedwait(pthread_cond_t* x, pthread_mutex_t* y, uint32_t secs) {
+int32_t SKIP_cond_timedwait(pthread_cond_t* x, pthread_mutex_t* y, uint32_t secs) {
   struct timeval tv;
   struct timespec ts;
   gettimeofday(&tv, NULL);
@@ -208,8 +208,8 @@ int SKIP_cond_timedwait(pthread_cond_t* x, pthread_mutex_t* y, uint32_t secs) {
   return pthread_cond_timedwait(x, y, &ts);
 }
 
-void SKIP_cond_broadcast(void* c) {
-  pthread_cond_broadcast(c);
+int32_t SKIP_cond_broadcast(void* c) {
+  return pthread_cond_broadcast(c);
 }
 
 /*****************************************************************************/
