2a434b4ab56a9ccf72c94ed102281e224b394ae6
LOPES Daniel <daniel@skiplabs.io>
2024-09-23 11:21:57 +0200

    Run all components together
    Remove skdb
    Manage subscribe
    Update tests

--- skipruntime-ts/ts/examples/sum.ts
+++ skipruntime-ts/ts/examples/sum.ts
@@ -4,32 +4,13 @@ import type {
   Mapper,
   EagerCollection,
   NonEmptyIterator,
-  SimpleSkipService,
-  SimpleServiceOutput,
-  JSONObject,
-  Writer,
+  SkipService,
+  Resource,
 } from "skip-runtime";
 
-import { runWithServer } from "skip-runtime";
+import { runWithRESTServer } from "skip-runtime";
 
-class Request implements Mapper<string, TJSON, string, TJSON> {
-  constructor(private source: EagerCollection<string, TJSON>) {}
-
-  mapElement(
-    key: string,
-    it: NonEmptyIterator<TJSON>,
-  ): Iterable<[string, TJSON]> {
-    const v = it.first() as JSONObject;
-    let computed = 0;
-    if (v.command == "add") {
-      const value = this.source.maybeGetOne(v.payload as string) as number;
-      computed = value ? value : 0;
-    }
-    return Array([key, computed]);
-  }
-}
-
-class Add implements Mapper<string, TJSON, string, TJSON> {
+class Plus implements Mapper<string, TJSON, string, TJSON> {
   constructor(private other: EagerCollection<string, TJSON>) {}
 
   mapElement(
@@ -45,49 +26,61 @@ class Add implements Mapper<string, TJSON, string, TJSON> {
   }
 }
 
-type Command = {
-  command: string;
-  payload: TJSON;
-};
+class Minus implements Mapper<string, TJSON, string, TJSON> {
+  constructor(private other: EagerCollection<string, TJSON>) {}
 
-type Set = { name: string; key: string; value: number };
-type Delete = { name: string; keys: string[] };
+  mapElement(
+    key: string,
+    it: NonEmptyIterator<TJSON>,
+  ): Iterable<[string, TJSON]> {
+    const v = it.first() as number;
+    const ev = this.other.maybeGetOne(key) as number;
+    if (ev !== null) {
+      return Array([key, v - (ev ?? 0)]);
+    }
+    return Array();
+  }
+}
 
-class Service implements SimpleSkipService {
-  name: string = "sum";
-  inputTables = ["input1", "input2"];
+class Add implements Resource {
+  reactiveCompute(
+    _store: SKStore,
+    collections: {
+      input1: EagerCollection<string, TJSON>;
+      input2: EagerCollection<string, TJSON>;
+    },
+  ): EagerCollection<string, TJSON> {
+    return collections.input1.map(Plus, collections.input2);
+  }
+}
 
-  async init(tables: Record<string, Writer<TJSON[]>>) {
-    console.log("Init called with tables", Object.keys(tables));
+class Sub implements Resource {
+  reactiveCompute(
+    _store: SKStore,
+    collections: {
+      input1: EagerCollection<string, TJSON>;
+      input2: EagerCollection<string, TJSON>;
+    },
+  ): EagerCollection<string, TJSON> {
+    return collections.input1.map(Minus, collections.input2);
+  }
+}
+
+class Service implements SkipService {
+  inputCollections = ["input1", "input2"];
+  resources = { add: Add, sub: Sub };
+
+  async init() {
+    console.log("Init called returns no values");
+    return {};
   }
 
   reactiveCompute(
     _store: SKStore,
-    requests: EagerCollection<string, TJSON>,
     inputCollections: Record<string, EagerCollection<string, TJSON>>,
-  ): SimpleServiceOutput {
-    const addResult = inputCollections.input1.map(Add, inputCollections.input2);
-    const output = requests.map(Request, addResult);
-    return {
-      output,
-      update: async (event: TJSON, writers: Record<string, Writer<TJSON>>) => {
-        const cmd = event as Command;
-        if (cmd.command == "set") {
-          const payload = cmd.payload as Set[];
-          for (const e of payload) {
-            const writer = writers[e.name];
-            writer.set(e.key, e.value);
-          }
-        } else if (cmd.command == "delete") {
-          const payload = cmd.payload as Delete[];
-          for (const e of payload) {
-            const writer = writers[e.name];
-            writer.delete(e.keys);
-          }
-        }
-      },
-    };
+  ) {
+    return inputCollections;
   }
 }
 
-runWithServer(new Service(), { port: 8081 });
+runWithRESTServer(new Service());
